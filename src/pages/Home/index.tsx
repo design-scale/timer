d62aec56cce85eafe0665fe79dee1996f57c2847
Modular Design <modulardesign@Mac-Mini.local>
2023-07-28 16:08:55 -0300

    Criando countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import { Button, HomeContainer, FormContainer, CountdownContainer, Separator, In
     
 import * as zod from 'zod'   
 import {zodResolver} from '@hookform/resolvers/zod'
+import { useState } from 'react';
 
 
 const newCycleFormValidationSchema = zod.object({
@@ -20,7 +21,17 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+    id: string
+    task: string
+    minutesAmount: number
+}
+
 export function Home(){
+
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0) 
 
     const {register, handleSubmit, watch, formState, reset} = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
@@ -35,9 +46,37 @@ export function Home(){
     // const [task, setTask] = useState('')
 
     function handleCreateNewCycle(data: NewCycleFormData){ 
+        const id = String(new Date().getTime())
+
+        const newCycle: Cycle = {
+            id, 
+            task: data.task,
+            minutesAmount: data.minutesAmount
+        }
+
+        setCycles([...cycles, newCycle])
+        setActiveCycleId(id)
+
         console.log(data)
         reset()
     }
+
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+    console.log(activeCycle)
+
+    const totalSeconds= activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60) 
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
+
+
+
+
     const task = watch('task')
     const isSubmitDisabled = !task
     return (
@@ -72,11 +111,11 @@ export function Home(){
                     <span>minutos</span>
                 </FormContainer>
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
                 <Button type="submit" disabled={isSubmitDisabled}>
