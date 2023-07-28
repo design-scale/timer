233bd7a06bd407017482e9fa1aef1f97143aafa5
Modular Design <modulardesign@Mac-Mini.local>
2023-07-28 13:55:10 -0300

    Validando o formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,25 @@
 import {useForm} from 'react-hook-form'
-
 import { Play } from "phosphor-react";
 import { Button, HomeContainer, FormContainer, CountdownContainer, Separator, Input, MinutesAmoutInput } from "./styles";
-import { useState } from "react";
+
+import {zodResolver} from '@hookform/resolvers/zod'
+import * as zod from 'zod'   
+
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "informe a tarefa"),
+    minutesAmount: zod.number()
+        .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos')
+})
 
 export function Home(){
 
-    const {register, handleSubmit, watch} = useForm()
+    const {register, handleSubmit, watch, formState} = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
+    
+    console.log(formState.errors)
 
     // const [task, setTask] = useState('')
 
@@ -41,7 +54,7 @@ export function Home(){
                         placeholder="00"
                         step={5}
                         min={5}
-                        max={60}
+                        // max={60}
                         {...register("minutesAmount", {valueAsNumber: true})}
                         />
                     <span>minutos</span>
